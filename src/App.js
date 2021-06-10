7ebf5a5729da63f6b4db65792e8d5e689d3041f3
Jason Belgard <belgard.jason@gmail.com>
2021-06-10 01:06:56 -0500

    commit for form v1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      email: "",
+      birthDate: "",
+      agree: true
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  render() {
+    return(
+      <div className = "App">
+        <form id = "contact-form" onSubmit = {this.handleSubmit.bind(this)} method = "POST">
+          <div className = "form-group">
+            <label htmlFor = "name">Name</label>
+            <input type = "text" className = "form-control" value = {this.state.name} onChange = {this.onNameChange.bind(this)} />
+          </div>
+          <div className = "form-group">
+            <label htmlFor = "exampleInputEmail1">Email</label>
+            <input type = "email" className = "form-control" aria-describedby = "emailHelp" value = {this.state.email} onChange = {this.onEmailChange.bind(this)} />
+          </div>
+          <div className = "form-group">
+            <label htmlFor = "birthDate">Birth Date</label>
+            <input type = "date" className = "birthdate" placeholder = "YYYY-MM-DD" data-date-split-input = "true" />
+          </div>
+          <div className = "form-group">
+            <label htmlFor = "agree">I agree to be contacted by email</label>
+            <input type = "checkbox" className = "agree" checked = {this.state.agree} onChange = {this.onAgreeChange.bind(this)} />
+          </div>
+          <input type = "submit" value = "Submit" />
+        </form>
+      </div>
+    )
+  }
+
+
+
+
 }
-
-export default App;
